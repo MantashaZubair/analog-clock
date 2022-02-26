1c51865c804c49b93efaa4c6148f05bb916d3fa9
Mantasha Zubair <zmantasha3@gmail.com>
2022-02-25 21:18:03 -0800

    build analog clock

---
+++ script.js
@@ -0,0 +1,20 @@
+myInterval = setInterval(setClock, 1000);
+clearInterval(myInterval);
+const hourHand = document.querySelector('[data-hour-hand]')
+const minuteHand = document.querySelector('[data-minute-hand]')
+const secondHand = document.querySelector('[data-second-hand]')
+function setClock(){
+    const currentDate =new Date()
+    const secondsRatio = currentDate.getSeconds() / 60 * 360;
+    secondHand.style.transform = 'rotate(20deg)'
+    const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60
+    const hoursRatio = (minutesRatio + currentDate.getHours())/12
+    setRotation(secondHand, secondsRatio)
+    setRotation(minuteHand, minutesRatio)
+    setRotation(hourHand, hoursRatio)
+    
+}
+function setRotation(element, rotationRatio){
+    element.style.setProperty('--rotation', rotationRatio * 360);
+}
+setClock();
