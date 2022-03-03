94fc0fdc9882d04256a9510cb521136f9e9ec7b4
Mantasha Zubair <zmantasha3@gmail.com>
2022-03-02 17:46:46 -0800

    updated index.html and add image also update script.js and style.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-myInterval = setInterval(setClock, 1000);
-clearInterval(myInterval);
+ setInterval(setClock, 1000);
+
 const hourHand = document.querySelector('[data-hour-hand]')
 const minuteHand = document.querySelector('[data-minute-hand]')
 const secondHand = document.querySelector('[data-second-hand]')
 function setClock(){
     const currentDate =new Date()
-    const secondsRatio = currentDate.getSeconds() / 60 * 360;
-    secondHand.style.transform = 'rotate(20deg)'
+    const secondsRatio = currentDate.getSeconds() / 60 
+   
     const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60
     const hoursRatio = (minutesRatio + currentDate.getHours())/12
     setRotation(secondHand, secondsRatio)
@@ -15,6 +15,6 @@ function setClock(){
     
 }
 function setRotation(element, rotationRatio){
-    element.style.setProperty('--rotation', rotationRatio * 360);
+    element.style.setProperty('--rotation', rotationRatio * 360)
 }
 setClock();
